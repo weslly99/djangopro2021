6f281f2adb4708e1793e67d3cb974cfb72ee4670
Weslly Oliveira <weslly99@hotmail.com>
2021-04-05 21:15:47 -0300

    Extração da chave secreta do arquivo de configuração do Django close #20

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "7v%l!spcedk(d2(2*wqm87wepr*qqx%!frh(%&t(!ml3gozc=x"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
