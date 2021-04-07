bf66b94332dbf2f103591701b32e23bf2702aec3
Weslly Oliveira <weslly99@hotmail.com>
2021-04-07 14:55:53 -0300

    fix flake8 extra espace

--- pypro/settings.py
+++ pypro/settings.py
@@ -76,15 +76,12 @@ WSGI_APPLICATION = "pypro.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-default_db_url = "sqlite:///" + str (BASE_DIR / "db.sqlite3")
+default_db_url = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
 
 DATABASES = {
     "default": config("DATABASE_URL", default=default_db_url, cast=parse_database)
 }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
