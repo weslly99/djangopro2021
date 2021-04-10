cb376d1d4ae22c54495fef8fe0780cb8f1687034
Weslly Oliveira <weslly99@hotmail.com>
2021-04-09 22:20:24 -0300

    Configuraçoes do Usuario close #27

--- pypro/settings.py
+++ pypro/settings.py
@@ -117,6 +117,7 @@ USE_L10N = True
 USE_TZ = True
 
 AUTH_USER_MODEL = "base.User"
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
